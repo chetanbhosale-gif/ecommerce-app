a4aeb4300b30b76b9855fff5b84927556ac83316
Chetan <chetan.bhosale@nonstopio.com>
2026-02-16 17:48:28 +0530

    Lombok used to improve code readability and optimization.

--- sb-ecom/sb-ecom/src/main/java/com/ecommerce/sb_ecom/model/Category.java
+++ sb-ecom/sb-ecom/src/main/java/com/ecommerce/sb_ecom/model/Category.java
@@ -4,36 +4,42 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Entity //@Entity(name="Categories") Sets name of table to categories.
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long categoryID;
     private String categoryName;
 
-    public Category(long categoryID, String categoryName) {
-        this.categoryID = categoryID;
-        this.categoryName = categoryName;
-    }
-    public Category(){
-
-    }
-
-    public long getCategoryID() {
-        return categoryID;
-    }
-
-    public void setCategoryID(long categoryID) {
-        this.categoryID = categoryID;
-    }
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
-    }
+//    public Category(long categoryID, String categoryName) {
+//        this.categoryID = categoryID;
+//        this.categoryName = categoryName;
+//    }
+//    public Category(){
+//
+//    }
+//
+//    public long getCategoryID() {
+//        return categoryID;
+//    }
+//
+//    public void setCategoryID(long categoryID) {
+//        this.categoryID = categoryID;
+//    }
+//
+//    public String getCategoryName() {
+//        return categoryName;
+//    }
+//
+//    public void setCategoryName(String categoryName) {
+//        this.categoryName = categoryName;
+//    }
 }
