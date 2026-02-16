44fb397cb58891395d6dcfdc8842c2e2854df174
Chetan <chetan.bhosale@nonstopio.com>
2026-02-16 15:22:57 +0530

    Spring Boot version conflict solved with H2 DB and used JPA .

--- sb-ecom/sb-ecom/src/main/java/com/ecommerce/sb_ecom/model/Category.java
+++ sb-ecom/sb-ecom/src/main/java/com/ecommerce/sb_ecom/model/Category.java
@@ -1,6 +1,11 @@
 package com.ecommerce.sb_ecom.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Category {
+    @Id
     private long categoryID;
     private String categoryName;
 
